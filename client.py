5ae1fe893dad353396facc4750ad04d2809b9a5f
張折睿 <a0979682372@gmail.com>
2023-10-18 22:44:30 +0800

    two socket for send and recv, work success but MTTCP fail

--- client.py
+++ client.py
@@ -9,36 +9,65 @@ import imutils
 import serial
 import threading
 
+def returnCameraIndexes():
+    # checks the first 10 indexes.
+    index = 0
+    arr = []
+    i = 10
+    while i > 0:
+        cap = cv2.VideoCapture(index)
+        if cap.read()[0]:
+            arr.append(index)
+            cap.release()
+        index += 1
+        i -= 1
+    return arr
+
+
+def testWebCAM(x):
+    cap = cv2.VideoCapture(x)
+    while(True):
+        ret, frame = cap.read()
+
+        # 將圖片轉為灰階
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 # receive data from server 
 def recv_thread():
     #python -m serial.tools.list_ports
-    global receiveWord,lock
+    # global receiveWord,lock
 
     COM_PORT = '/dev/ttyACM0'
     BAUD_RATES=115200
     ser = serial.Serial(COM_PORT,BAUD_RATES)
     client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket2.connect(('192.168.20.31', 10101))
-    client_socket2.setblocking(False)
+    client_socket2.connect(('192.168.20.21', 20101))
+    # client_socket2.setblocking(False)
+    tmp=bytes('Unknown Person\n',encoding='utf8')
     try:
         while 1:
             recv_data=client_socket2.recv(1024)
-            print(recv_data.decode('utf8'))
             print(recv_data)
-        #     tmp=bytes('Unknow Person',encoding='utf8')
-            ser.write(recv_data)
-        #     ser.write(tmp)
+            # print(recv_data.decode('utf8'))
+            if (recv_data==tmp):
+                print('enter ALARM !!!')
+                ser.write(tmp)
     except Exception as err:
+        print('thread-Receive result:')
         print(err)
 
 # as client to send data to server
 def client_thread():
-    global receiveWord,lock
+    # global receiveWord,lock
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # client_socket.connect(('0.tcp.ngrok.io', 19194))
-    client_socket.connect(('192.168.20.31', 10100))
+    client_socket.connect(('192.168.20.21', 20100))
     # client_socket.setblocking(False)
 
     cam = cv2.VideoCapture(0)
@@ -73,6 +102,7 @@ def client_thread():
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         except Exception as err:
+            print('thread-Receive result:')
             print(err)
             print('-------end the video stream------')
             break
@@ -83,8 +113,10 @@ def client_thread():
 
 
 # global var
-receiveWord='empty'
-lock = threading.Lock()
+# receiveWord='empty'
+# lock = threading.Lock()
+
+
 
 
 first_thread = threading.Thread(target = client_thread)
@@ -95,3 +127,6 @@ second_thread.start()
 
 first_thread.join()
 second_thread.join()
+
+# testWebCAM(0)
+print('End main process')
